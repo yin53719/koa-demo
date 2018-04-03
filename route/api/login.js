8f429ffcfce8eb341023b16ae8afec9d1ab6c072
hardy <1111@qq.com>
2018-04-03 23:34:44 +0800

    'file'

--- route/api/login.js
+++ route/api/login.js
@@ -115,4 +115,92 @@ router.get('/getMenuList',async (ctx)=>{
   }
   ctx.body=array;
 })
+router.get('/getMenuList/vue',async (ctx)=>{
+    let sql = `SELECT * FROM tc_menu_copy`;
+    let result = await query.selectAllData(sql)
+    var array=[];
+    for(var i=0;i<result.length;i++){
+       let newV=result[i];
+       let p=newV.parent;
+       var len=p.toString().length;
+       var c=newV.menu_id.toString().slice(0,len);
+       var leaf=true;
+       if(newV.leaf==0){
+          leaf=false;
+       }
+       var hidden=true;
+       if(newV.hidden==1){
+           hidden=false;
+       }
+       //取出一级菜单
+       if(p== 0){
+          
+              let obj={
+                  id:newV.menu_id,
+                  name:newV.name,
+                  children:[],
+                  hidden:hidden,
+                  leaf:leaf,
+                  path:newV.path,
+                  meta:{
+                    icon:newV.icon,
+                    title:newV.title
+                  },
+                  redirect:'noredirect',
+                  component:newV.component
+              }
+              array.push(obj);
+          
+       }
+       //取出二级菜单
+       if(len == 4){
+          for(var j=0;j<array.length;j++){
+            if(p==array[j].id){
+                  let obj={
+                      id:newV.menu_id,
+                      name:newV.name,
+                      children:[],
+                      hidden:hidden,
+                      leaf:leaf,
+                      path:newV.path,
+                      meta:{
+                        icon:newV.icon,
+                        title:newV.title
+                      },
+                      component:newV.component
+                  }
+                  array[j].children.push(obj);
+            }
+          }
+       }
+       //取出三级菜单
+       if(len == 6){
+        for(var h=0;h<array.length;h++){
+          for(var k=0;k<array[h].children.length;k++){
+            if(p==array[h].children[k].id){
+                let obj={
+                    id:newV.menu_id,
+                    name:newV.name,
+                    children:[],
+                    hidden:hidden,
+                    leaf:leaf,
+                    path:newV.path,
+                    meta:{
+                        icon:newV.icon,
+                        title:newV.title
+                    },
+                    component:newV.component
+                }
+                array[h].children[k].children.push(obj);
+              }
+            }
+            }
+        }
+        //取出四级菜单
+        if(len == 8){
+  
+        }
+    }
+    ctx.body=array;
+  })
 module.exports = router;
